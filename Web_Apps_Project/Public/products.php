eedd041493280a8166fcebf6b301816b909b0aae
ricardo-jm <122681314+ricardo-jm@users.noreply.github.com>
2024-04-19 22:01:25 +0100

    update products page
    update products page

--- Web_Apps_Project/Public/products.php
+++ Web_Apps_Project/Public/products.php
@@ -26,16 +26,17 @@ FROM product";
     <div class="container-fluid py-4">
         <div class="row bg-secondary justify-content-center text-center align-items-center text-white pt-3">
             <?php foreach ($result as $products) { ?>
-                <div class="col-md-4 pet-list-item">
+                <div class="col-lg-4 col-sm-6">
+                    <img src="/Web_Apps_Project/Public/images/<?php echo $products['image']; ?>" class="img-rounded img-sales img-thumbnail">
                     <h2>
                         <?php echo $products['prodname']; ?>
                     </h2>
-                    <blockquote class="pet-details">
+
                         <h5><?php echo $products['category']; ?></span></h5>
                         <h5><?php echo 'Description: '. $products['proddescription']; ?></h5>
                         <h5><?php echo 'Price: '. $products['price']; ?>€</h5>
-                    </blockquote>
-                    <img src="/Web_Apps_Project/Public/images/<?php echo $products['image']; ?>" class="img-rounded img-sales img-thumbnail">
+
+
                 </div>
             <?php } ?>
         </div>
