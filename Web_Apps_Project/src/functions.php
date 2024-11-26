00bef86097809d8184ae20f5c6ca1f7b408d1d96
ricardo-jm <122681314+ricardo-jm@users.noreply.github.com>
2024-11-26 14:18:22 +0000

    Update functions.php

--- Web_Apps_Project/src/functions.php
+++ Web_Apps_Project/src/functions.php
@@ -51,14 +51,6 @@ function register($connection)
         $sql = sprintf("INSERT INTO %s (%s) values (%s)", "user", implode(", ",
             array_keys($new_user)), ":" . implode(", :", array_keys($new_user)));
 
-        //$sql = "INSERT INTO user (username, password, email, phone) 
-        //        VALUES (:username, :password, :email, :phone)";
-
-        //$statement->bindParam(':username', $new_user['username']);
-        //$statement->bindParam(':password', $new_user['pwd']);
-        //$statement->bindParam(':email', $new_user['email']);
-        //$statement->bindParam(':phone', $new_user['phone']);
-
         $statement = $connection->prepare($sql);
         $statement->execute($new_user);
     } catch (PDOException $error) {
